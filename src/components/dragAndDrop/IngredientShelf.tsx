a343978aa01c15d3025d95d77a491081dedd784f
jchack1 <55161597+jchack1@users.noreply.github.com>
2025-08-05 23:29:27 -0600

    Split ingredients into multiple shelves on smaller screens

--- src/components/dragAndDrop/IngredientShelf.tsx
+++ src/components/dragAndDrop/IngredientShelf.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
+import { useState, useEffect } from "react";
 import { ingredients } from "../../recipes";
 import { Ingredient } from "./Ingredient";
 
-const IngredientArea = styled.div`
+const IngredientRow = styled.div`
   display: flex;
   align-items: end;
+  flex-direction: row;
 `;
-
 const Shelf = styled.div`
   width: 100%;
   height: 15px;
@@ -20,14 +21,46 @@ const IngredientShelfContainer = styled.div`
 `;
 
 export const IngredientShelf = () => {
+  const [chunkedIngredients, setChunkedIngredients] = useState<string[][]>([
+    ingredients,
+  ]);
+
+  useEffect(() => {
+    const updateChunks = () => {
+      if (window.innerWidth <= 425) {
+        // chunk into groups on smaller screens
+        const chunkSize = 3;
+        const chunks = [];
+
+        for (let i = 0; i < ingredients.length; i += chunkSize) {
+          chunks.push(ingredients.slice(i, i + chunkSize));
+        }
+        setChunkedIngredients(chunks);
+      } else {
+        // one row on large screens, so just one 'chunk'
+        setChunkedIngredients([ingredients]);
+      }
+    };
+
+    updateChunks();
+    window.addEventListener("resize", updateChunks);
+    return () => window.removeEventListener("resize", updateChunks);
+  }, []);
+
   return (
     <IngredientShelfContainer>
-      <IngredientArea>
-        {ingredients.map((ingredient) => (
-          <Ingredient ingredient={ingredient} />
-        ))}
-      </IngredientArea>
-      <Shelf />
+      {/* on small screens, ingredients split into multiple shelves */}
+      {chunkedIngredients.map((chunk, index) => (
+        <div key={index}>
+          <IngredientRow>
+            {chunk.map((ingredient) => (
+              <Ingredient ingredient={ingredient} />
+            ))}
+          </IngredientRow>
+
+          <Shelf />
+        </div>
+      ))}
     </IngredientShelfContainer>
   );
 };
